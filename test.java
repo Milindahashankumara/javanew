38b8d3d4c76b578e2695bac7d397a7f82b0152a7
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2024-11-18 06:49:05 +0530

    inherit

--- test.java
+++ test.java
@@ -1,4 +1,34 @@
+import java.util.Scanner ;
+
 public class test{
 
-    
-}
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        Author author = new Author("Mili","british");
+
+        System.out.println("Author details");
+        author.displayauthor();
+
+        System.out.println();
+
+    }
+
+    }
+
+    class Author{
+
+        String name;
+        String nation;
+
+        public Author(String name, String nation){
+            this.name = name;
+            this.nation = nation;
+        }
+
+        public void displayauthor(){
+            System.out.println(name);
+            System.out.println(nation);
+        }
+    }
